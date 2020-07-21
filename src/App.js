61d92826b1a4ae6e5ad7e3ea8078afd016530c32
Anas Alam <anasalam027@gmail.com>
2020-07-21 23:09:35 +0530

    Website Created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Components/CSS/App.css'
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './Components/Home'
+import Projects from './Components/Projects'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  render() {
+    return(
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route exact path='/projects' component={Projects}></Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
